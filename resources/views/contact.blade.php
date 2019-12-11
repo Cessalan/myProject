34b9fc73c4ec9fd87a8b19c4199cb7f42b11771c
Cessalan <31516973+Cessalan@users.noreply.github.com>
2019-12-11 11:12:36 -0500

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fatsy
+ * Date: 2019-12-11
+ * Time: 10:57 AM
+ */
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title> GCC</title>
+</head>
+<body>
+@yield('content')
+@section('sidebar')
+<div>
+    this is a sidebar
+</div>
+</body>
+</html>
+
